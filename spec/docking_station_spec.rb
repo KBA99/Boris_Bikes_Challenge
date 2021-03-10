166acf399b4e7ed4ef7de04d2aa20222da0dd5b5
Kwabena <67422830+KBA99@users.noreply.github.com>
2021-03-10 14:45:26 +0000

    Excercise 10 Complete

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,13 @@
+require 'docking_station.rb'
+
+describe DockingStation do
+
+  # it 'Can release bike' do
+  #   expect(DockingStation.new).to respond_to(:release_bike)
+  # end
+
+  it 'responds to release_bike' do
+    expect(subject).to respond_to :release_bike
+  end
+
+end
