0072ea42cf79036b62d05720834cead00cd67b6a
Kwabena <67422830+KBA99@users.noreply.github.com>
2021-03-12 11:05:21 +0000

    Boris Bikes part 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,7 +5,7 @@ describe DockingStation do
   # it 'Can release bike' do
   #   expect(DockingStation.new).to respond_to(:release_bike)
   # end
-
+  
   it 'responds to release_bike' do
     expect(subject).to respond_to :release_bike
   end
