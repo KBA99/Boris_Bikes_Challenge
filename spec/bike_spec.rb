849f651b12866a79728e798912b696526116f8ef
Kwabena <67422830+KBA99@users.noreply.github.com>
2021-03-12 14:23:51 +0000

    Boris Bikes part 11

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,4 +1,8 @@
 require 'bike.rb'
 
 describe Bike do
+  it "responds to :working?" do
+    is_expected.to respond_to(:working?)
+  end
+  # it {  is_expected.to respond_to :working?}
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,7 +7,17 @@ describe DockingStation do
   # end
   
   it 'responds to release_bike' do
-    expect(subject).to respond_to :release_bike
+    expect(subject).to respond_to(:release_bike)
   end
 
+  it 'docks something' do
+    bike = subject
+    expect(subject.dock(bike)).to eq(bike)
+  end
+
+  it 'returns docked bike' do
+    bike = subject
+    subject.dock(bike)
+    expect(subject.bike).to eq(bike)
+  end
 end
